415e559e7b47e1fbb3326008bda6f41fa09fa18f
Vimal Mudalagi <vimalmudalagi123@gmail.com>
2024-03-03 13:39:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL)
+  return (
+    <>
+      <h1>A blog with appwrite</h1>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+

bcc69e640fda45401626ed95895bf70e09583b41
Vimal Mudalagi <vimalmudalagi123@gmail.com>
2024-03-04 09:35:57 +0530

    authentification in appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,66 @@
+import conf from "../conf/conf";
+import { Client, Account } from "appwrite";
+
+export class AuthService {
+    client = new this.client();
+    account;
+
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
+        this.account = new Account(this.client);
+
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUserv:: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUserv:: error", error);
+        }
+    }
+
+}
+
+
+const authService = new AuthService();
+
+
+export default AuthService;
+
+
